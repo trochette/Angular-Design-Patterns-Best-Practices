395cc7a6069e4ed021b8569f335f2761c7590805
Tommy Rochette <julo@Tommys-MacBook-Pro.local>
2013-06-05 21:30:25 -0400

    Merging master to gh-pages

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
 
 angular.module('notes', ['navigation','highlightjs','notifications','notes.slide','notes.NotesService','notes.NotesModel'])
     .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/intro', {templateUrl: 'partials/slide1.html', controller:SlideController});
         $routeProvider.otherwise({redirectTo: '/intro'});
 
         //This one is not exactly a good practice but its the
